2752b4c83a06ccd854b76d4d0a5b648416a04f95
Mariia S <thimariia@gmail.com>
2023-07-04 08:29:02 +0300

    добавили новую возможность к классу Pen - pen_type

--- main.py
+++ main.py
@@ -6,25 +6,30 @@ class Pencil:
         print(f'Нарисован рисунок цветом {self.color}')
     
 class Pen(Pencil):
-    def __init__(self, color):
+    def __init__(self, color, pen_type):
         super().__init__(color)
+        self.pen_type = pen_type
     
     def draw(self):
         super().draw()
     
     def sign(self):
-        if self.color == 'синий' or self.color == 'чёрный' or self.color == 'фиолетовый':
+        if self.color == 'синий' or self.color == 'чёрный' or self.color == 'фиолетовый' and self.pen_type != 'гелевая':
             print(f'Подписан документ')
+        elif self.pen_type == 'гелевая':
+            print(f'Ручкой типа {self.pen_type} нельзя подпиать')
         else:
             print(f'Ручкой цвета {self.color} нельзя подписать документ')
 
 
-BluePen = Pen('синий')
-BluePen.sign()
-RedPen = Pen('красный')
+RedPen = Pen('красный', 'гелевая')
 RedPen.sign()
-PinkPen = Pen('розовый')
+PinkPen = Pen('розовый', 'шариковая')
 PinkPen.sign()
 PinkPen.draw()
 BlackPencil = Pencil('черный')
 BlackPencil.draw()
+BluePencil = Pencil('синий')
+BluePencil.draw()
+BluePen = Pen('синий', 'шариковая')
+BluePen.sign()
